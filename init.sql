c13e4764cfdde9c7a205ee28e92900dda3bc2694
Thomas Andersen <coolfatboy02@gmail.com>
2023-10-12 10:46:20 +0200

    SÅDAN

--- init.sql
+++ init.sql
@@ -3,5 +3,5 @@ CREATE DATABASE my_database;
 
 -- Create an admin user and grant privileges
 CREATE USER 'THOMAS'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON my_database.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON my_database.* TO 'THOMAS'@'%';
 FLUSH PRIVILEGES;
