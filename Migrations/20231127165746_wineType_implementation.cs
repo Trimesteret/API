7a6e379d641011f5e986ef6da9f76b7d7359cad9
Christoffer Gamél <christoffer.gamel@gmail.com>
2023-11-27 18:09:53 +0100

    post edit

---
+++ Migrations/20231127165746_wineType_implementation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class wineType_implementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Items",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WineType",
+                table: "Items",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WineType",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Items",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
