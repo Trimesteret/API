b441e0fc163bcfcf9c41abf810e1ebdf2cb5b29f
Thomas Andersen <coolfatboy02@gmail.com>
2023-12-09 12:12:00 +0100

    SW-131 * Edit enum finished

--- Enums/CustomEnum.cs
+++ Enums/CustomEnum.cs
@@ -6,4 +6,19 @@ public class CustomEnum
     public EnumType EnumType { get; set; }
     public string Key { get; set; }
     public string Value { get; set; }
+
+    /// <summary>
+    /// Empty constructor for Entity Framework
+    /// </summary>
+    public CustomEnum()
+    {
+
+    }
+
+    public CustomEnum(EnumType enumType, string key, string value)
+    {
+        EnumType = enumType;
+        Key = key;
+        Value = value;
+    }
 }
--- Models/Suppliers/Supplier.cs
+++ Models/Suppliers/Supplier.cs
@@ -1,9 +1,6 @@
-using API.Models.Items;
-
 namespace API.Models.Suppliers;
 
 public class Supplier
 {
     public int Id { get; set; }
-    public Item[] Items { get; set; }
 }
